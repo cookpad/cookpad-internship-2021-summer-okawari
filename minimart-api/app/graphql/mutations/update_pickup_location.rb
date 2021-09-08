46a74f5d5714bea6e55c9b520304c831db29c208
yamori-masato <yamorimasato@gmail.com>
2021-09-08 03:57:06 +0000

    [Hands-on 3]: 商品の受け取り場所設定を作る(Mutation)

---
+++ minimart-api/app/graphql/mutations/update_pickup_location.rb
@@ -0,0 +1,13 @@
+module Mutations
+  class UpdatePickupLocation < BaseMutation
+    field :pickup_location, Types::PickupLocationType, null: true
+
+    argument :pickup_location_id, ID, required: true
+
+    def resolve(pickup_location_id:)
+      pickup_location = PickupLocation.find_by(id: pickup_location_id)
+      context[:current_user].update(pickup_location: pickup_location)
+      { pickup_location: pickup_location }
+    end
+  end
+end
