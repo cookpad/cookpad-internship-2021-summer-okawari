7d950458b4f8567cc3b3e89386095ddd1500b6b7
task4233 <29667656+task4233@users.noreply.github.com>
2021-09-08 03:00:08 +0000

    feat: GraphQLのQueryにPickupLocationの設定を追加

--- minimart-api/app/graphql/types/query_type.rb
+++ minimart-api/app/graphql/types/query_type.rb
@@ -7,24 +7,33 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    field :viewer, Types::UserType, 'リクエストしたユーザー自身を返す', null: true
+    field :viewer, Types::UserType, "リクエストしたユーザー自身を返す", null: true
+
     def viewer
       # context は GraphqlController で定義されている
       context[:current_user]
     end
 
     # すべての商品を返すクエリ Products の定義
-    field :products, [ProductType], 'すべての商品を返す', null: false
+    field :products, [ProductType], "すべての商品を返す", null: false
+
     def products
       Product.all
     end
 
     # 指定されたIDの商品を返すクエリ Product(id: ID!) の定義
-    field :product, ProductType, '指定されたIDの商品を返す', null: true do
+    field :product, ProductType, "指定されたIDの商品を返す", null: true do
       argument :id, ID, required: true
     end
+
     def product(id:)
       Product.find_by(id: id)
+    end
+
+    field :pickup_locations, [PickupLocationType], "すべての受け取り場所を返す", null: false
+
+    def pickup_locations
+      PickupLocation.all
     end
   end
 end
