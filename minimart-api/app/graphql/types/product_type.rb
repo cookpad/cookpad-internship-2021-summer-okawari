0247ee581466c0a4052dc04e678d13f7c59ff816
yamori-masato <yamorimasato@gmail.com>
2021-09-08 03:04:57 +0000

    [Hands-on 1]: Productにfield(description, price)を追加

--- minimart-api/app/graphql/types/product_type.rb
+++ minimart-api/app/graphql/types/product_type.rb
@@ -2,6 +2,8 @@ module Types
   class ProductType < Types::BaseObject
     field :id, ID, null: false
     field :name, String, null: false
+    field :description, String, null: false
+    field :price, Int, null: false
     field :image_url, String, null: false
     def image_url
       "http://localhost:3001#{object.image_path}"
