f898cf379e7373316215c44e5d6767b35f47b6ba
task4233 <29667656+task4233@users.noreply.github.com>
2021-09-08 03:39:41 +0000

    feat: pickupLocationの編集機能用のmutationsを追加

---
+++ minimart-api/app/graphql/mutations/update_pickup_location.rb
@@ -0,0 +1,16 @@
+module Mutations
+  class UpdatePickupLocation < BaseMutation
+    # 返り値のフィールドの設定
+    field :pickup_location, Types::PickupLocationType, null: true
+
+    # 引数の設定
+    argument :pickup_location_id, ID, required: true
+
+    # リゾルバの設定(このエンドポイントがよばれたときに動く部分?)
+    def resolve(pickup_location_id:)
+      pickup_location = PickupLocation.find_by(id: pickup_location_id)
+      context[:current_user].update(pickup_location: pickup_location)
+      { pickup_location: pickup_location }
+    end
+  end
+end
--- minimart-api/app/graphql/types/mutation_type.rb
+++ minimart-api/app/graphql/types/mutation_type.rb
@@ -1,10 +1,11 @@
 module Types
   class MutationType < Types::BaseObject
+    field :update_pickup_location, mutation: Mutations::UpdatePickupLocation
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
+    # field :test_field, String, null: false,
+    #   description: "An example field added by the generator"
+    # def test_field
+    #   "Hello World"
+    # end
   end
 end
